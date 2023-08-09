9ca46a4050aeac0683bb4fa6833bcae56dc76e59
zh-lx <18366276315@163.com>
2023-08-09 08:43:08 +0800

    feat: add wrapper code

---
+++ react/wc-element-react/src/ZhButton.ts
@@ -0,0 +1,20 @@
+
+ import * as React from 'react';
+ import {createComponent} from '@lit-labs/react';
+ 
+ import {ZhButton as ZhButtonElement} from 'wc-element/dist\ZhButton.js';
+ 
+ 
+ 
+
+ 
+ export const ZhButton = createComponent(
+   React,
+   'undefined',
+   ZhButtonElement,
+   {
+     
+   }
+ );
+ 
+ 
